50b6b8f971e314dba3f1cd33acb528d6c5bf5fa4
leopoldhub <leopold12.hub@gmail.com>
2021-01-27 22:23:09 +0100

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,10 @@
-console.log("getMessage()", getMessage.toString());
-console.log("createElementFromHTML(string)", createElementFromHTML.toString());
-console.log("appendDiv(id)", appendDiv.toString());
-console.log("appendDivIfDontExist(id)", appendDivIfDontExist.toString());
-console.log("replaceDivIfExist(id)", replaceDivIfExist.toString());
-console.log("loadScript(url)", loadScript.toString());
+/*
+run this script to check the integrity of injected functions
+*/
+
+console.log(getMessage.toString());
+console.log(createElementFromHTML.toString());
+console.log(appendDiv.toString());
+console.log(appendDivIfDontExist.toString());
+console.log(replaceDivIfExist.toString());
+console.log(loadScript.toString());
