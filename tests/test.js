74d587e6a40e8158fd68195b4bbeae651acd44f8
leopoldhub <leopold12.hub@gmail.com>
2021-01-27 22:12:50 +0100

    Create test.js

---
+++ tests/test.js
@@ -0,0 +1,6 @@
+console.log("getMessage()", getMessage.toString());
+console.log("createElementFromHTML(string)", createElementFromHTML.toString());
+console.log("appendDiv(id)", appendDiv.toString());
+console.log("appendDivIfDontExist(id)", appendDivIfDontExist.toString());
+console.log("replaceDivIfExist(id)", replaceDivIfExist.toString());
+console.log("loadScript(url)", loadScript.toString());
